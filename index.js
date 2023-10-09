a4ff194510667d3c1a009c651b3bf4db4963d308
Joses Syanwa <josesyanwa@gmail.com>
2023-10-09 19:54:19 +0300

    Completed Assignments

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // code your solution here
+const record = [
+    { year: "2015", result: "W"},
+    { year: "2014", result: "N/A"},
+    { year: "2013", result: "L"},
+    //...
+  ];
+  
+  function superbowlWin(record) {
+    const win = record.find(game => game.result === "W");
+    return win ? win.year : undefined;
+  }
+  
+  // Example usage:
+  const superBowlYear = superbowlWin(record);
+  if (superBowlYear !== undefined) {
+    console.log(`The Denver Broncos won the Superbowl in ${superBowlYear}`);
+  } else {
+    console.log("The Denver Broncos have not won the Superbowl.");
+  }
+  
